692fda91a85cff8f8568542c39856a19b85329c1
Arthur Gabriel <122728865+arthur-Gsilva@users.noreply.github.com>
2024-09-10 19:54:40 -0300

    corrigindo para build

--- src/components/dashboards/Alunobox.tsx
+++ src/components/dashboards/Alunobox.tsx
@@ -1,5 +1,5 @@
 import { IoMdInformationCircleOutline } from "react-icons/io"
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card"
+import { Card, CardContent, CardHeader } from "../ui/card"
 import { TooltipContent, TooltipProvider, TooltipTrigger } from "@radix-ui/react-tooltip"
 import { Tooltip } from "../ui/tooltip"
 
